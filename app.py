3caf0a762c6d7df092a17e4d7a45f9602c490e8c
Arthur Henrique Nogueira <22502890@aluno.cotemig.com.br>
2026-06-09 13:45:09 -0300

    Add files via upload
    Atividade Jinja

---
+++ app.py
@@ -0,0 +1,65 @@
+from flask import Flask, jsonify, request
+import dados
+import funcoes
+
+def menu():
+
+
+        app = Flask(__name__)
+        biblioteca = dados.carregar_do_arquivo()
+
+        @app.route('/')
+        def hello():
+            return "Hello World!"
+
+        @app.route('/name/<nome>', methods=['GET', 'POST'])
+        def name(nome):
+            return f"Olá, eu não sou o {nome}!"
+
+        @app.route('/biblioteca', methods=['GET'])
+        def lista_livros():
+            return jsonify(biblioteca), 200
+
+        @app.route('/biblioteca/<isbn>')
+        def detalha_livro(isbn):
+            for l in biblioteca:
+                if l['isbn'] == isbn:
+                    return jsonify(1)
+            return jsonify ("message: livro não localizado"),404
+
+        @app.route('/biblioteca/insert', methods=['POST'])
+        def insere_livro():
+            novo_livro = request.get_json()
+            biblioteca.append(novo_livro)
+            dados.salvar_no_arquivo(biblioteca)
+            return "", 204
+
+        @app.route('/biblioteca/delete/<isbn>', methods = ['DELETE'])
+        def detalha_livro(isbn):
+            for l in biblioteca:
+                if l['isbn'] == isbn:
+                    biblioteca.remove(l)
+                    dados.salvar_no_arquivo(biblioteca)
+                    return jsonify ("message: livro deletado com sucesso"),200
+            return jsonify("message: livro não localizado"),404
+
+        @app.route('/biblioteca/update/<isbn>', methods = ['PUT', 'POST'])
+        def atualiza_livro(isbn):
+            for l in biblioteca:
+                if l['isbn'] == isbn:
+                    novo_livro = request.get_json()
+                    biblioteca.remove(l)
+                    biblioteca.append(novo_livro)
+                    dados.salvar_no_arquivo(biblioteca)
+                    return jsonify ("message: livro alterado com sucesso"),200
+            return jsonify("message: livro não localizado"),404
+
+
+        
+
+        # if __name__ == "__main__":
+        #     exibir_menu()
+
+
+        if __name__ == '__main__':
+            app.run(debug=True)
